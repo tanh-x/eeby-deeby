8e825827e877deddf80f53e685f9a1c9f1b05504
Thanh <kewber0@gmail.com>
2023-05-19 03:15:32 -0700

    trying

--- scripts/ui/battle/EntityOverlay.kt
+++ scripts/ui/battle/EntityOverlay.kt
@@ -5,43 +5,52 @@ import battle.entity.Vulnerable
 import godot.Control
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
+import godot.core.Vector2
+import godot.global.GD
 import utils.helpers.instantiateScene
 import utils.helpers.node
 import kotlin.random.Random
 
 @RegisterClass
 open class EntityOverlay : Control() {
-	internal lateinit var entity: AbstractEntity<*>
+    internal lateinit var entity: AbstractEntity<*>
 
-	private var entIsVulnerable: Boolean = false
+    private var entIsVulnerable: Boolean = false
 
-	private lateinit var healthbar: Healthbar
+    private lateinit var healthbar: Healthbar
 
-	@RegisterFunction
-	override fun _ready() {
-		healthbar = node("Healthbar")
-	}
+    @RegisterFunction
+    override fun _ready() {
+        healthbar = node("Healthbar")
+    }
 
-	internal open fun attachEntity(entity: AbstractEntity<*>) {
-		this.entity = entity
-		if (entity is Vulnerable) {
-			healthbar.entity = entity
-			entIsVulnerable = true
-			healthbar.visible = true
-		}
+    @RegisterFunction
+    override fun _canDropData(position: Vector2, data: Any?): Boolean = (data == "attack")
 
-		updateAll()
-	}
+    @RegisterFunction
+    override fun _dropData(position: Vector2, data: Any?) {
+        GD.print("successful attack")
+    }
 
-	@RegisterFunction
-	open fun updateAll() {
-		if (entIsVulnerable) healthbar.rerender()
-	}
+    internal open fun attachEntity(entity: AbstractEntity<*>) {
+        this.entity = entity
+        if (entity is Vulnerable) {
+            healthbar.entity = entity
+            entIsVulnerable = true
+            healthbar.visible = true
+        }
+        updateAll()
+    }
 
-	internal fun spawnDamageNumber(number: Double) {
-		val label: DamageNumber = instantiateScene("res://scenes/ui/DamageNumber.tscn")
-		label.text = number.toInt().toString()
-		label.rectRotation = Random.nextDouble() * 60 - 30
-		addChild(label)
-	}
+    @RegisterFunction
+    open fun updateAll() {
+        if (entIsVulnerable) healthbar.rerender()
+    }
+
+    internal fun spawnDamageNumber(number: Double) {
+        val label: DamageNumber = instantiateScene("res://scenes/ui/DamageNumber.tscn")
+        label.text = number.toInt().toString()
+        label.rectRotation = Random.nextDouble() * 60 - 30
+        addChild(label)
+    }
 }
--- scripts/ui/battle/PlayerOverlay.kt
+++ scripts/ui/battle/PlayerOverlay.kt
@@ -3,20 +3,32 @@ package ui.battle
 import battle.entity.AbstractEntity
 import character.AbstractCharacter
 import character.AbstractCharacterNode
+import godot.GlobalConstants.BUTTON_LEFT
+import godot.InputEvent
+import godot.InputEventMouseButton
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
+import godot.core.Vector2
 
 @RegisterClass
 class PlayerOverlay : EntityOverlay() {
-	private lateinit var character: AbstractCharacter<out AbstractCharacterNode>
+    private lateinit var character: AbstractCharacter<out AbstractCharacterNode>
+    private var isDragging: Boolean = false
 
-	@RegisterFunction
-	override fun _ready() {
-		super._ready()
-	}
+    @RegisterFunction
+    override fun _ready() {
+        super._ready()
+    }
 
-	override fun attachEntity(entity: AbstractEntity<*>) {
-		super.attachEntity(entity)
-		this.character = entity as AbstractCharacter<out AbstractCharacterNode>
-	}
+    @RegisterFunction
+    override fun _getDragData(position: Vector2): Any? {
+        println("hello")
+        if (getRect().hasPoint(position)) return "attack"
+        return null
+    }
+
+    override fun attachEntity(entity: AbstractEntity<*>) {
+        super.attachEntity(entity)
+        this.character = entity as AbstractCharacter<out AbstractCharacterNode>
+    }
 }
